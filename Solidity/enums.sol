802f695e89aa809e655078608ea3ce60579eca46
its-himanshu-13 <himanshuagrawal8477@gmail.com>
2023-12-04 10:29:58 +0530

    enums files created comments added in it

---
+++ Solidity/enums.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract enums{
+
+    enum Status{
+        Pending, //0
+        Shipped, //1
+        Accepted, //2
+        Rejected, //3
+        Cancel //4
+    }
+    Status status; //By Default 0
+    //Following function is called thee getter function which fetch the value of a varaible
+    function getstatus() public view returns(status){
+        return status;
+    }
+    function setstatus( Status _status) public {
+        status = _status;
+    }
+    functin
+}
