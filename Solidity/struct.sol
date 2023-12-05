7ff73e2215e9ee4b96c9907bb5303fca50309056
its-himanshu-13 <himanshuagrawal8477@gmail.com>
2023-12-05 10:10:31 +0530

    npm install solc command is configured success

--- Solidity/struct.sol
+++ Solidity/struct.sol
@@ -2,13 +2,13 @@
 pragma solidity ^0.8.0;
 
 contract Struct {
-    Struct Emp{
+    struct Emp{
         string name;
         uint age;
         address acc;
     }
     Emp public emp;
-    constructor(string _name, uint _age, address  _acc){
+    constructor(string memory _name, uint _age, address  _acc){
         emp.name = _name;
         emp.age = _age;
         emp.acc = _acc;
@@ -18,19 +18,29 @@ contract Struct {
     function setValues() public {
         // If we are using inside the function the memory keyword will be used
         Emp memory emp1 = Emp("himanshu",19, 0x71C7656EC7ab88b098defB751B7401B5f6d8976F);
-        Emp memory emp2 = Emp("Amogh",21, 0x71C7656EC7ab88b098defB751B7401B5f6d8076F);
+        Emp memory emp2 = Emp("Amogh",21, 0x71C7656Ec7AB88b098DEFb751B7401B5f6D8076F);
         Emp memory emp3 = Emp({acc:msg.sender, name:"Chirag", age:19});
         //Another way of initializing
         Emp memory emp4;
         emp4.name = "Chetan";
         emp4.age = 18;
-        emp4.acc = 0x71C7656EC7ab88b098defB751A7401B5f6d8976F;
+        emp4.acc = 0x71c7656Ec7aB88b098dEFb751A7401b5f6d8976F;
         // pushing values into array 
         emps.push(emp1);
         emps.push(emp2);
         emps.push(emp3);
+        emps.push(emp4);
 
         emps.push(Emp("Gauri",21, msg.sender));
+        // Let's Create a Temporary variable and try to change the value of emp
+        // // 1. If we use memory keyword then it will not change the value of emp
+        // Emp memory emp_temp = emp;
+        // emp_temp.name = "Himanshu Agrawal";
+        // 2. If we use storage keyword then it will change the value of emp
+        Emp storage emp_temp = emp;
+        emp_temp.name = "Himanshu Agrawal";
+
+
     }
 
 }
